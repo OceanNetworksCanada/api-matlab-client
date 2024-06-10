f0465f7164b2cf6b666d0f74201de3a23dff6488
Isla Li <islali0904@gmail.com>
2024-06-10 16:37:03 -0700

    issue-21: Add version check function and update initializer to run checkVersion at the beginning

---
+++ onc/Contents.m
@@ -0,0 +1,21 @@
+% Ocean Networks Canada API Client Library
+% Version 2.2.0 10-Jun-2024
+%
+% Functions
+%   +onc/OncDiscovery          - Contains the functionality that wraps the API discovery services
+%   +onc/OncDelivery           - Contains the functionality that wraps the API data product delivery services. To be inherited by the Onc class
+%   +onc/OncRealTime           - Contains the functionality that wraps API real-time services.
+%   +onc/OncArchive            - Contains the functionality that wraps API archivefile services
+%
+% Live Scripts
+%   examples/OncDiscoveryLocations.mat        - Example Usage of OncDiscovery Location Service 
+%   examples/OncDiscoveryProperties.mat       - Example Usage of OncDiscovery Properties Service 
+%   examples/OncDiscoveryDataProducts.mat     - Example Usage of OncDiscovery DataProducts Service
+%   examples/OncDiscoveryDeployments.mat      - Example Usage of OncDiscovery Deployments Service            
+%   examples/OncDiscoveryDeviceCategories.mat - Example Usage of OncDiscovery Device Categories Service
+%   examples/OncDiscoveryDevices.mat          - Example Usage of OncDiscovery Devices Service
+%   examples/OncRealTime.mat                  - Example Usage of OncRealTime Service
+%   examples/OncDeliveryDataProducts.mat      - Example Usage of OncDelivery Service
+%   examples/OncArchive.mat                   - Example Usage of OncArchive Service
+%
+% Copyright 2024, ONC Data Team
