01ad33c45d46a2a094bc747075d58d3397a7e328
Isla Li <islali0904@gmail.com>
2024-06-18 10:59:41 -0700

    issue-21: fix typos and add links to documentation

--- onc/Contents.m
+++ onc/Contents.m
@@ -1,6 +1,8 @@
 % Ocean Networks Canada API Client Library
 % Version 2.2.0 10-Jun-2024
-%
+% 
+% For documentation click or type directly in the command window: <a href="matlab: doc ONC Toolbox">doc ONC Toolbox</a>.
+% 
 % Functions
 %   +onc/OncDiscovery          - Contains the functionality that wraps the API discovery services
 %   +onc/OncDelivery           - Contains the functionality that wraps the API data product delivery services. To be inherited by the Onc class
@@ -8,14 +10,14 @@
 %   +onc/OncArchive            - Contains the functionality that wraps API archivefile services
 %
 % Live Scripts
-%   examples/OncDiscoveryLocations.mat        - Example Usage of OncDiscovery Location Service 
-%   examples/OncDiscoveryProperties.mat       - Example Usage of OncDiscovery Properties Service 
-%   examples/OncDiscoveryDataProducts.mat     - Example Usage of OncDiscovery DataProducts Service
-%   examples/OncDiscoveryDeployments.mat      - Example Usage of OncDiscovery Deployments Service            
-%   examples/OncDiscoveryDeviceCategories.mat - Example Usage of OncDiscovery Device Categories Service
-%   examples/OncDiscoveryDevices.mat          - Example Usage of OncDiscovery Devices Service
-%   examples/OncRealTime.mat                  - Example Usage of OncRealTime Service
-%   examples/OncDeliveryDataProducts.mat      - Example Usage of OncDelivery Service
-%   examples/OncArchive.mat                   - Example Usage of OncArchive Service
+%   examples/OncDiscoveryLocations.mlx        - Example Usage of OncDiscovery Location Service 
+%   examples/OncDiscoveryProperties.mlx       - Example Usage of OncDiscovery Properties Service 
+%   examples/OncDiscoveryDataProducts.mlx     - Example Usage of OncDiscovery DataProducts Service
+%   examples/OncDiscoveryDeployments.mlx      - Example Usage of OncDiscovery Deployments Service            
+%   examples/OncDiscoveryDeviceCategories.mlx - Example Usage of OncDiscovery Device Categories Service
+%   examples/OncDiscoveryDevices.mlx          - Example Usage of OncDiscovery Devices Service
+%   examples/OncRealTime.mlx                  - Example Usage of OncRealTime Service
+%   examples/OncDeliveryDataProducts.mlx      - Example Usage of OncDelivery Service
+%   examples/OncArchive.mlx                   - Example Usage of OncArchive Service
 %
 % Copyright 2024, ONC Data Team
